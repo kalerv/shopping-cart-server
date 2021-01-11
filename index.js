b4d8192efa85f2dfa1ba39536b577ca42002753e
Vikas Kaler <Vikas.kaler@mobilelive.ca>
2021-01-10 23:51:14 -0500

    added script

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use((req, rs, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // parse application/json
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
