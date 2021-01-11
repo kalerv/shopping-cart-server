e5756d5c4c5e8b6fed121583f8420038946eff29
Vikas Kaler <Vikas.kaler@mobilelive.ca>
2021-01-10 23:52:38 -0500

    cors header added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use((req, rs, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
